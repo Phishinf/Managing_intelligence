93bfefd5f0b6df10256840208bf54d2d325f9ff9
Victorius Hung <135007842+Phishinf@users.noreply.github.com>
2025-07-03 08:31:42 +0800

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import MultiAIConversationManager from '@/components/MultiAIConversationManager';
+
+export default function Home() {
+  return (
+    <main className="h-screen w-full">
+      <MultiAIConversationManager />
+    </main>
+  );
+}
